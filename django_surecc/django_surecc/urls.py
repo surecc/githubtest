9a057322906cf38ff315880d1693e475bb18ebe5
surecc <bianca.chan.cc@gmail.com>
2013-01-13 01:46:17 +0800

    add the view of current time

--- django_surecc/django_surecc/urls.py
+++ django_surecc/django_surecc/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 # from django.conf.urls.defaults import *
-from django_surecc.views import hello
+from django_surecc.views import hello, current_datetime
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -17,4 +17,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
     ('^hello/$', hello),
+    ('^time/$', current_datetime),
 )
