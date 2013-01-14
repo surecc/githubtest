8a46b682c64d9e49c522c89f5d8dc0f171de4d31
surecc <bianca.chan.cc@gmail.com>
2013-01-14 16:37:54 +0800

    show simple Error warning to users

--- django_surecc/django_surecc/books/views_books.py
+++ django_surecc/django_surecc/books/views_books.py
@@ -5,18 +5,25 @@ from django_surecc.books.models import Book
 def search_form(request):
     return render_to_response('search_form.html')
 
+#the simplest way to search
 def search_try(request):
     if 'q' in request.GET:
         message = 'You searched for: %r' % request.GET['q']
     else:
         message = 'You submitted an empty form.'
     return HttpResponse(message)
-        
+ 
+#better way to search       
 def search(request):
+    error = False
     if 'q' in request.GET:
         q = request.GET['q']
-        books = Book.objects.filter(title__icontains=q)
-        return render_to_response('search_results.html', 
+        if not q:
+            error = True
+        elif len(q) > 20:
+            error = True
+        else:
+            books = Book.objects.filter(title__icontains=q)
+            return render_to_response('search_results.html', 
                                   {'books':books, 'query':q})
-    else:
-        return HttpResponse('Please submit a search term.')
+    return render_to_response('search_form.html',{'error':error})
